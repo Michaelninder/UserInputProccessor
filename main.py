4807830ba8f7d1a66151063cd142d76dc41b8d04
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2025-03-03 16:13:35 +0100

    Update main.py
    Hobby Question

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def get_user_input():
   name = input("What's your name? ")
   age = input("How old are you? ")
   favorite_color = input("What's your favorite color? ")  
+  hobby = input("What's your favorite hobby? ")
   
   return name, age, favorite_color
 
