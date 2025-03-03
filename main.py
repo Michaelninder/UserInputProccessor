0e1434141b78df99b50144481e523ee5377e480f
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2025-03-03 16:10:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ def get_user_input():
   name = input("What's your name? ")
   age = input("How old are you? ")
   favorite_color = input("What's your favorite color? ")  
+  
+  return name, age, favorite_color
